70461233e7f6d3c4bae02169cb227431e927a826
Puga.isaias <64232550+pugaIsaias@users.noreply.github.com>
2021-06-14 16:29:11 -0600

    changed names to my project

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * _s functions and definitions
+ *Nimble_Style functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package _s
+ * @package Nimble-Style
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'NIMBLE_STYLE_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'NIMBLE_STYLE_VERSION', '1.0.0' );
 }
 
-if ( ! function_exists( '_s_setup' ) ) :
+if ( ! function_exists( 'nimble_style_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,14 +20,14 @@ if ( ! function_exists( '_s_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function _s_setup() {
+	function nimble_style_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on _s, use a find and replace
-		 * to change '_s' to the name of your theme in all the template files.
+		 * If you're building a theme based onNimble-Style, use a find and replace
+		 * to change 'nimble_style' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'nimble_style', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -50,7 +50,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', '_s' ),
+				'menu-1' => esc_html__( 'Primary', 'nimble_style' ),
 			)
 		);
 
@@ -75,7 +75,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 		add_theme_support(
 			'custom-background',
 			apply_filters(
-				'_s_custom_background_args',
+				'nimble_style_custom_background_args',
 				array(
 					'default-color' => 'ffffff',
 					'default-image' => '',
@@ -102,7 +102,7 @@ if ( ! function_exists( '_s_setup' ) ) :
 		);
 	}
 endif;
-add_action( 'after_setup_theme', '_s_setup' );
+add_action( 'after_setup_theme', 'nimble_style_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +111,22 @@ add_action( 'after_setup_theme', '_s_setup' );
  *
  * @global int $content_width
  */
-function _s_content_width() {
-	$GLOBALS['content_width'] = apply_filters( '_s_content_width', 640 );
+function nimble_style_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'nimble_style_content_width', 640 );
 }
-add_action( 'after_setup_theme', '_s_content_width', 0 );
+add_action( 'after_setup_theme', 'nimble_style_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function _s_widgets_init() {
+function nimble_style_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', '_s' ),
+			'name'          => esc_html__( 'Sidebar', 'nimble_style' ),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', '_s' ),
+			'description'   => esc_html__( 'Add widgets here.', 'nimble_style' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,22 +134,22 @@ function _s_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', '_s_widgets_init' );
+add_action( 'widgets_init', 'nimble_style_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( '_s-style', 'rtl', 'replace' );
+function nimble_style_scripts() {
+	wp_enqueue_style( 'nimble-style-style', get_stylesheet_uri(), array(), NIMBLE_STYLE_VERSION );
+	wp_style_add_data( 'nimble-style-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'nimble-style-navigation', get_template_directory_uri() . '/js/navigation.js', array(), NIMBLE_STYLE_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts' );
+add_action( 'wp_enqueue_scripts', 'nimble_style_scripts' );
 
 /**
  * Implement the Custom Header feature.
